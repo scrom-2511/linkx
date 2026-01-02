a456da32f737e63514f8b8c58be79fbee34055bd
scrom <formain337@gmail.com>
2026-01-02 19:21:31 +0000

    add: dark theming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
+import { ThemeProvider } from "./components/theme-provider";
 import Encrypted from "./pages/Encrypted";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div className="bg-black h-screen w-screen bg-gradient-to-r from-[#262626] to-black flex justify-center items-center">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="h-screen w-screen bg-primary-foreground flex justify-center items-center">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/encrypted" element={<Encrypted />} />
         </Routes>
-    </div>
+      </div>
+    </ThemeProvider>
   );
 };
 
