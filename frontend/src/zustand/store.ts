2761ada627119e3d07e2100828cb9440e1c86b69
scrom <formain337@gmail.com>
2026-01-03 20:20:37 +0000

    modify: removed title and description from store, as now it is handled by an array in the component itself

--- frontend/src/zustand/store.ts
+++ frontend/src/zustand/store.ts
@@ -1,24 +1,12 @@
 import { create } from 'zustand'
 
 export type CurrentFeatureStore = {
-    title: string;
-    description: string;
     index: number;
-    setTitle: (title: string) => void;
-    setDescription: (description: string) => void;
     setIndex: (index: number) => void;
 };
 
 export const useCurrentFeatureStore = create<CurrentFeatureStore>((set)=>({
     index: 0,
-    title: "shorten url",
-    description: "Enter your url to shorten it",
-    setTitle: (title) => set((state)=>({
-        title: title
-    })),
-    setDescription: (description) => set((state)=>({
-        description: description
-    })),
     setIndex: (index) => set((state)=>({
         index: index
     }))
