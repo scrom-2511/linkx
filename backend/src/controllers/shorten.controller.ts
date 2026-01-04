8f9b468e04298a46a9387c4e1582d6645d2cc2f7
scrom <formain337@gmail.com>
2026-01-04 18:45:01 +0000

    fix: this.code

---
+++ backend/src/controllers/shorten.controller.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import { shortLinkGenerator } from "../utils/shortLinkGenerator.util";
+import { shortenLinkType } from "../types/shortenLink.type";
+import { ShortenLinkModel } from "../model/shortLink.model";
+import { ERROR_CODES, HttpStatus } from "../types";
+
+export const shortenLink = async (req: Request, res: Response) => {
+  try {
+    const validateData = shortenLinkType.safeParse(req.body);
+
+    if (!validateData.success) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        success: false,
+        error: {
+          code: ERROR_CODES.INVALID_INPUT.code,
+          message: ERROR_CODES.INVALID_INPUT.message,
+        },
+      });
+    }
+
+    const dataExists = await ShortenLinkModel.findOne({
+      link: validateData.data.link,
+    });
+
+    if (dataExists) {
+      return res.status(HttpStatus.OK).json({
+        success: true,
+        data: {
+          shortenedLink: dataExists.shortenedLink,
+        },
+      });
+    }
+
+    let newShortLink = "s-";
+
+    while (true) {
+      newShortLink = newShortLink + shortLinkGenerator();
+      if (await ShortenLinkModel.findOne({ shotenedLink: newShortLink })) {
+        newShortLink = "s-";
+      } else {
+        break;
+      }
+    }
+
+    await ShortenLinkModel.create({ ...req.body, shortenedLink: newShortLink });
+
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      data: {
+        shortenedLink: newShortLink,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      error: {
+        code: ERROR_CODES.INTERNAL_SERVER_ERROR.code,
+        message: ERROR_CODES.INTERNAL_SERVER_ERROR.message,
+      },
+    });
+  }
+};
