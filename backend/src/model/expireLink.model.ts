44ce702c4abe09fb472afe10b6015976c67f6e6a
scrom <formain337@gmail.com>
2026-01-04 18:46:40 +0000

    fix: this.code

---
+++ backend/src/model/expireLink.model.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const LinkSchema = new mongoose.Schema(
+  {
+    link: { type: String, required: true },
+    dateAndTime: { type: Date, required: true },
+    expirerLink: { type: Date, required: true },
+  },
+  { timestamps: true }
+);
+
+export const ExpireLinkModel = mongoose.model("ExpireLink", LinkSchema);
