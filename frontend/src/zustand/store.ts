aa9ca089ccf059704e0959da7cbfb7daa3908cbe
scrom <formain337@gmail.com>
2026-01-04 09:51:24 +0000

    add: current input url store

--- frontend/src/zustand/store.ts
+++ frontend/src/zustand/store.ts
@@ -1,13 +1,27 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 export type CurrentFeatureStore = {
-    index: number;
-    setIndex: (index: number) => void;
+  index: number;
+  setIndex: (index: number) => void;
 };
 
-export const useCurrentFeatureStore = create<CurrentFeatureStore>((set)=>({
-    index: 0,
-    setIndex: (index) => set((state)=>({
-        index: index
-    }))
-}))
+export const useCurrentFeatureStore = create<CurrentFeatureStore>((set) => ({
+  index: 0,
+  setIndex: (index) =>
+    set((state) => ({
+      index: index,
+    })),
+}));
+
+export type CurrentInputUrlStore = {
+  currentInputUrl: string;
+  setCurrentInputUrl: (url: string) => void;
+};
+
+export const useCurrentInputUrlStore = create<CurrentInputUrlStore>((set) => ({
+  currentInputUrl: "",
+  setCurrentInputUrl: (url) =>
+    set((state) => ({
+      currentInputUrl: url,
+    })),
+}));
