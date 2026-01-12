5ecd0d6fc12d4e2e9eb238f85b9c24f624a02e8c
Scrom <formain337@gmail.com>
2026-01-12 15:18:52 +0530

    Update app.config.ts

--- frontend/src/config/app.config.ts
+++ frontend/src/config/app.config.ts
@@ -1,3 +1,3 @@
-export const FRONTEND_URL = "http://localhost:5173";
+export const FRONTEND_URL = "https://linkx-4to.pages.dev/";
 export const BACKEND_URL = "https://linkx-backend.vercel.app/";
 export const BACKEND_URL_API = "https://linkx-backend.vercel.app/api";
