84d2ace1bc0c39c7adb7b5e81a4a3c9aed8a8aae
Scrom <formain337@gmail.com>
2026-01-12 15:17:54 +0530

    Update app.config.ts

--- frontend/src/config/app.config.ts
+++ frontend/src/config/app.config.ts
@@ -1,3 +1,3 @@
 export const FRONTEND_URL = "http://localhost:5173";
-export const BACKEND_URL = "http://localhost:3000/";
-export const BACKEND_URL_API = "http://localhost:3000/api";
+export const BACKEND_URL = "https://linkx-backend.vercel.app/";
+export const BACKEND_URL_API = "https://linkx-backend.vercel.app/api";
